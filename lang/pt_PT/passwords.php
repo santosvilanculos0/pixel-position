1f0beabb0d46d0c1fca84055038e014eb01faf0f
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-26 16:23:21 +0200

    refactor: configure project

---
+++ lang/pt_PT/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'A palavra-passe foi redefinida!',
+    'sent' => 'O lembrete para a palavra-passe foi enviado!',
+    'throttled' => 'Por favor aguarde, antes de tentar novamente.',
+    'token' => 'Este código de recuperação da palavra-passe é inválido.',
+    'user' => 'Não existe nenhum utilizador com o e-mail indicado.',
+
+];
