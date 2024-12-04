40c2ef1ce93dc471893a5b84b46769940ad4b502
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-04 10:26:40 +0200

    fix: some times empy user is returned in callback route

--- passport/routes/api.php
+++ passport/routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Api\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// WARNING: Don't remove, or alter, socialite passport provider relies on this route to get user,
+// and the middleware must be 'auth:api'
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware(['client']);
+})->middleware('auth:api');
 
 Route::apiResource('jobs', JobController::class)->middleware(['client']);
